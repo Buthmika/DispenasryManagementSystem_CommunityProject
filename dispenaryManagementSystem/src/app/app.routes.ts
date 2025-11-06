a33ca6630ad7aa27c3b70b1ced8adce25d429fc8
Dampitige DBM <buthmikamenushan@gmail.com>
2025-11-06 16:21:01 +0530

    Add login component and update routing
    Introduced a new LoginComponent with associated HTML and CSS files. Updated app routes to include the login path and set it as the default redirect. Modified app.html to use router-outlet and cleaned up app.ts imports. Also fixed RegisterComponent test setup in register.spec.ts.

--- dispenaryManagementSystem/src/app/app.routes.ts
+++ dispenaryManagementSystem/src/app/app.routes.ts
@@ -1,9 +1,9 @@
 import { Routes } from '@angular/router';
-
+import { LoginComponent } from './Components/login/login';
 import { RegisterComponent } from './Components/register/register';
 
 export const routes: Routes = [
 	{ path: '', redirectTo: 'login', pathMatch: 'full' },
-	
+	{ path: 'login', component: LoginComponent },
 	{ path: 'register', component: RegisterComponent }
 ];
--- dispenaryManagementSystem/src/app/app.ts
+++ dispenaryManagementSystem/src/app/app.ts
@@ -1,10 +1,9 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { RegisterComponent } from './Components/register/register';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,RegisterComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrls: ['./app.css']
 })
