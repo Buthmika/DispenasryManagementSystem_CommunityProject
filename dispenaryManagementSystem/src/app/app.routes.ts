3282662aa664c4252174cbad2094d72f1bb534cb
Ashani <ashanitharukshi2002@gmail.com>
2025-11-12 17:10:10 +0530

    Add patient management route to application routing

--- dispenaryManagementSystem/src/app/app.routes.ts
+++ dispenaryManagementSystem/src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './Components/login/login';
 import { RegisterComponent } from './Components/register/register';
+import { PatientListComponent } from './Components/patientManagement/patientList/patientList';
 
 export const routes: Routes = [
 	{ path: '', redirectTo: 'login', pathMatch: 'full' },
 	{ path: 'login', component: LoginComponent },
-	{ path: 'register', component: RegisterComponent }
+	{ path: 'register', component: RegisterComponent },
+	{ path: 'patient-management', component: PatientListComponent },
 ];
