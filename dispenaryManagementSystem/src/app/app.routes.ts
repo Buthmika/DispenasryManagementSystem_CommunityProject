8f44b8252b210f3a9fe91fdb4777c61edea3aa02
Ashani <ashanitharukshi2002@gmail.com>
2025-11-15 23:09:56 +0530

    Add route for PatientForm component in app routing

--- dispenaryManagementSystem/src/app/app.routes.ts
+++ dispenaryManagementSystem/src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './Components/login/login';
 import { RegisterComponent } from './Components/register/register';
+import { PatientFormComponent } from './Components/patientManagement/components/patient-form/patient-form';
 
 export const routes: Routes = [
 	{ path: '', redirectTo: 'login', pathMatch: 'full' },
 	{ path: 'login', component: LoginComponent },
 	{ path: 'register', component: RegisterComponent },
+	{path:'patientForm', component: PatientFormComponent}
 
 ];
