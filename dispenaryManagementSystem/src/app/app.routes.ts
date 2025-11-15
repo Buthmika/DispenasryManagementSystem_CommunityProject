a587fcb9990e4069650bad2c6b53d6ab19ddf8ef
Ashani <ashanitharukshi2002@gmail.com>
2025-11-16 00:29:33 +0530

    Add route for PatientList component in app.routes.ts

--- dispenaryManagementSystem/src/app/app.routes.ts
+++ dispenaryManagementSystem/src/app/app.routes.ts
@@ -2,11 +2,13 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './Components/login/login';
 import { RegisterComponent } from './Components/register/register';
 import { PatientFormComponent } from './Components/patientManagement/components/patient-form/patient-form';
+import { PatientListComponent } from './Components/patientManagement/components/patient-list/patient-list';
 
 export const routes: Routes = [
 	{ path: '', redirectTo: 'login', pathMatch: 'full' },
 	{ path: 'login', component: LoginComponent },
 	{ path: 'register', component: RegisterComponent },
-	{path:'patientForm', component: PatientFormComponent}
+	{path:'patientForm', component: PatientFormComponent},
+	{path:'patientList', component: PatientListComponent}
 
 ];
