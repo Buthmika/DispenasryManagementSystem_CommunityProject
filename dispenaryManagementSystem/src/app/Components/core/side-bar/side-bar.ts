6a55ab24bc8256a0ca46eac8736e0aba5e4914eb
Ashani <ashanitharukshi2002@gmail.com>
2025-11-16 12:52:50 +0530

    Add sidebar component with logo and navigation menu

---
+++ dispenaryManagementSystem/src/app/Components/core/side-bar/side-bar.ts
@@ -0,0 +1,14 @@
+import { R } from '@angular/cdk/keycodes';
+import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-side-bar',
+  imports: [MatIconModule, RouterLink, RouterLinkActive, RouterOutlet],
+  templateUrl: './side-bar.html',
+  styleUrls: ['./side-bar.css'],
+})
+export class SideBar {
+
+}
